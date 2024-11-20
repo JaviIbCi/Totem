5eadff1db11519f0bdace006e1dd665de640ef37
Micky <102488779+Mickypipe@users.noreply.github.com>
2024-11-20 00:46:19 -0300

    Revert "subi las apps"
    This reverts commit 1b734cc28c56891c34491ead7f9cc53e169507d8.

---
+++ Fase 2/Evidencias Proyecto/Aplicación/totem_administrador/server/chunk-NEDADJCY.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+var e={production:!1,apiKey:"epRNlvPmJwpDZ8ZEsQbH3PG06ImnrS",apiUrl:"https://backend.v2.totemvespucio.cl/api/admin/"};export{e as a};
--- Fase 2/Evidencias Proyecto/Aplicación/totem_administrador/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiKey: 'epRNlvPmJwpDZ8ZEsQbH3PG06ImnrS',
-    apiUrl: 'https://backend.v2.totemvespucio.cl/api/admin/',
-    //apiUrl: 'http://localhost:4201/api/admin/'
-};
